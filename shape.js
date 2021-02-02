008b2901cd8f9cdff60ad43be203df339a50a52f
emansoliman <emyrade29@gmail.com>
2021-02-02 02:32:55 +0200

    last update

--- shape.js
+++ shape.js
@@ -1,11 +1,19 @@
 imge=document.getElementsByTagName("img")[0]
 show_img=document.getElementsByTagName("i")[0]
 show_img.addEventListener("click",showimge);
+var flag=true
 function showimge(e){
 
-
+    if(flag){
     imge.style.display="block"
+    flag=false
+    }
+    else{
+    imge.style.display="none"
+    flag=true
+    }
 }
+ 
 
 
 
